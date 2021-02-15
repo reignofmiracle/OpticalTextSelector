cd5a69a6e821a22bdbda5ff67b86b370cef47782
reign <reignofmiracle@gmail.com>
2021-02-16 00:02:30 +0900

    working on wpf

---
+++ OpticalTextSelector/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace OpticalTextSelector
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            var viewModel = new MainViewModel();
+            viewModel.window = this;
+            viewModel.canvas = canvas;
+
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ OpticalTextSelector/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace OpticalTextSelector
+{
+    public class MainViewModel
+    {
+        KeyboardHook keyboardHook = new KeyboardHook();
+
+        bool isActive = false;
+
+        public MainViewModel()
+        {
+            this.keyboardHook.events
+                .Where(e => e != null && e.Key == Keys.W && e.Control && e.IsKeyDown)
+                .Throttle(TimeSpan.FromMilliseconds(50))
+                .ObserveOnDispatcher()
+                .Subscribe(e =>
+            {
+                this.isActive = !this.isActive;
+
+                if (this.isActive)
+                {
+                    this.Background.Value = new SolidColorBrush(Color.FromArgb(1, 0, 0, 0));
+                }
+                else
+                {
+                    this.Background.Value = new SolidColorBrush(Colors.Transparent);
+                }
+            });            
+        }
+
+        public Window window { get; set; }
+        public Canvas canvas { get; set; }
+
+        public ReactivePropertySlim<Brush> Background { get; } = new ReactivePropertySlim<Brush>();
+    }
+}
